90dddc5b758ce95fb5af5b5aa424af6e36cbed9d
Koval0050 <121100028+Koval0050@users.noreply.github.com>
2023-08-19 16:59:31 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,244 @@
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { WrapperPhonebook } from './components/Phonebookwrapper/phonebookwrapper';
+import { Title } from './components/Title/title';
+import { InputForm } from './components/Addform/addform';
+import { ContactsTitle } from './components/Contacttitle/contactTitle';
+import { Search } from './components/Search/search';
+import { ContactList } from './components/Contactslist/contactList';
+import { ShowButton } from './components/ShowButton/showButton';
+import { CountMessage } from './components/CountMessage/CountMessage';
+import { getCountMessage } from './utils/getCountMessage';
+import { save, load } from './utils/localStorageJSON';
+
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [buttonVisible, setButtonVisible] = useState(true);
+
+  //збереження у localStorage
+  useEffect(() => {
+    save('contacts', contacts);
+  }, [contacts]);
+
+  //завантажуємо контакти якщо такі є
+  useEffect(() => {
+    const savedContacts = load('contacts');
+    if (savedContacts) {
+      setContacts(savedContacts);
+    }
+  }, []);
+
+  const handleAddContact = newContactData => {
+    const newContactEntity = {
+      id: nanoid(),
+      ...newContactData,
+    };
+
+    checkNewContactPresence(newContactEntity.name)
+      ? alert(`${newContactEntity.name} is already in contacts!`)
+      : setContacts(prev => [...prev, newContactEntity]);
+    save('contacts', contacts); // This line might be unnecessary
+  };
+
+  const handleDeleteContact = contactId => {
+    setContacts(prev => prev.filter(contact => contact.id !== contactId));
+    if (contacts.length === 1) {
+      setButtonVisible(prev => !prev);
+    }
+  };
+
+  const handleFilterContactsByName = ({ target: { value } }) => {
+    setFilter(value);
+  };
+
+  const checkNewContactPresence = contactName => {
+    return contacts.some(contact => contact.name === contactName);
+  };
+
+  const handleToggleSecondButtonVisibility = () => {
+    setButtonVisible(prev => !prev);
+    console.log('click');
+  };
+
+  const contactsFilteredByName = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <WrapperPhonebook>
+      <Title text="PhoneBook" />
+
+      {buttonVisible && (
+        <InputForm
+          addContact={handleAddContact}
+          // contacts={contacts}
+          // onUpdateContact={setContacts}
+        />
+      )}
+      {buttonVisible && (
+        <CountMessage text={getCountMessage(contacts.length)} />
+      )}
+      {contacts.length !== 0 && (
+        <ShowButton
+          contactCount={contacts.length}
+          onButtonChange={handleToggleSecondButtonVisibility}
+        ></ShowButton>
+      )}
+      {!buttonVisible && (
+        <>
+          <ContactsTitle text="Contacts" />
+          <Search
+            filter={filter}
+            onChange={handleFilterContactsByName}
+            // valueSearch={searchText}
+            // onChange={changeFilter}
+            text={'Find contacts by name'}
+          />
+          {/* {filterContacts.length === 0 ? ( */}
+          {/* <p>There are no contacts matching the search criteria</p> */}
+          {/* ) : ( */}
+          <ContactList
+            contacts={contactsFilteredByName}
+            onDelete={handleDeleteContact}
+            // contacts={filterContacts}
+            // onDeleteContact={handleDeleteContact}
+          />
+        </>
+      )}
+    </WrapperPhonebook>
+  );
+};
+
+// import React from "react";
+// import { nanoid } from "nanoid";
+// import { WrapperPhonebook } from "./components/Phonebookwrapper/phonebookwrapper";
+// import { Title } from "./components/Title/title";
+// import { InputForm } from "./components/Addform/addform";
+// import { ContactsTitle } from "./components/Contacttitle/contactTitle";
+// import { Search } from "./components/Search/search";
+// import { ContactList } from "./components/Contactslist/contactList";
+// import { ShowButton } from "./components/ShowButton/showButton";
+// import { CountMessage } from "./components/CountMessage/CountMessage";
+// import { getCountMessage } from "./utils/getCountMessage";
+// import { save, load } from "./utils/localStorageJSON";
+
+// export class App extends React.Component {
+//   state = {
+//     contacts: [
+//       // { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//       // { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//       // { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//       // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+//     ],
+//     filter: "",
+//     buttonVisible: true,
+//   };
+
+//   componentDidUpdate(prevState) {
+//     if (prevState.contacts !== this.state.contacts) {
+//       save("contacts", this.state.contacts);
+//     }
+//   }
+//   componentDidMount() {
+//     const savedContacts = load("contacts");
+//     if (savedContacts) {
+//       this.setState({ contacts: savedContacts });
+//     }
+//   }
+
+//   handleAddContact = (newContactData) => {
+//     const newContactEntity = {
+//       id: nanoid(),
+//       ...newContactData,
+//     };
+
+//     this.checkNewContactPresence(newContactEntity.name)
+//       ? alert(`${newContactEntity.name} is already in contacts!`)
+//       : this.setState((state) => ({
+//           contacts: [...state.contacts, newContactEntity],
+//         }));
+//     save("contacts", this.state.contacts);
+//   };
+
+// handleDeleteContact = (contactId) => {
+//   this.setState((state) => ({
+//     contacts: state.contacts.filter((contact) => contact.id !== contactId),
+//   }));
+//   if (this.state.contacts.length === 1) {
+//     this.setState((prevState) => {
+//       return {
+//         buttonVisible: !prevState.buttonVisible,
+//       };
+//     });
+//   }
+// };
+
+// handleFilterContactsByName = ({ target: { value } }) => {
+//   this.setState(() => ({ filter: value }));
+// };
+
+// checkNewContactPresence = (contactName) => {
+//   return this.state.contacts.some((contact) => contact.name === contactName);
+// };
+
+// handleToggleSecondButtonVisibility = () => {
+//   this.setState((prevState) => {
+//     return {
+//       buttonVisible: !prevState.buttonVisible,
+//     };
+//   });
+//   console.log("click");
+//   // setFilterContacts(contacts);
+// };
+
+//   render() {
+//     const { contacts, filter, buttonVisible } = this.state;
+//     const contactsFilteredByName = contacts.filter((contact) =>
+//       contact.name.toLowerCase().includes(filter.toLowerCase())
+//     );
+
+//     return (
+//       <WrapperPhonebook>
+//         <Title text="PhoneBook" />
+//         {buttonVisible && (
+//           <InputForm
+//             addContact={this.handleAddContact}
+//             // contacts={contacts}
+//             // onUpdateContact={setContacts}
+//           />
+//         )}
+//         {buttonVisible && (
+//           <CountMessage text={getCountMessage(contacts.length)} />
+//         )}
+//         {contacts.length !== 0 && (
+//           <ShowButton
+//             contactCount={contacts.length}
+//             onButtonChange={this.handleToggleSecondButtonVisibility}
+//           ></ShowButton>
+//         )}
+//         {!buttonVisible && (
+//           <>
+//             <ContactsTitle text="Contacts" />
+//             <Search
+//               filter={filter}
+//               onChange={this.handleFilterContactsByName}
+//               // valueSearch={searchText}
+//               // onChange={changeFilter}
+//               text={"Find contacts by name"}
+//             />
+//             {/* {filterContacts.length === 0 ? ( */}
+//             {/* <p>There are no contacts matching the search criteria</p> */}
+//             {/* ) : ( */}
+//             <ContactList
+//               contacts={contactsFilteredByName}
+//               onDelete={this.handleDeleteContact}
+//               // contacts={filterContacts}
+//               // onDeleteContact={handleDeleteContact}
+//             />
+//           </>
+//         )}
+//       </WrapperPhonebook>
+//     );
+//   }
+// }
