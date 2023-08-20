ed3cf0ea450fd43726953e40e3c5fc4548bcaf7d
Koval0050 <121100028+Koval0050@users.noreply.github.com>
2023-08-20 18:51:41 +0300

    hw-06

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
-import { App } from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
