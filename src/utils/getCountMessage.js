6ccdc069332939b6bf9a258f12a14907a551b2c6
Koval0050 <121100028+Koval0050@users.noreply.github.com>
2023-08-21 21:06:02 +0300

    fix

--- src/utils/getCountMessage.js
+++
@@ -1,9 +0,0 @@
-export function getCountMessage(contactCount) {
-  if (contactCount === 0) {
-    return "You don't have contacts";
-  } else if (contactCount === 1) {
-    return "You have 1 contact";
-  } else {
-    return `You have ${contactCount} contacts`;
-  }
-}
